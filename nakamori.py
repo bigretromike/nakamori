2148353ab68b77bf0d5cb1bc2fbc8823c9bd0bd0
BigRetroMike <bigretromike@users.noreply.github.com>
2015-11-23 14:49:09 +0100

    1.0.8

--- nakamori.py
+++ nakamori.py
@@ -1,26 +1,14 @@
 import urllib2, re, urllib, json, sys, os.path, time, base64, datetime
-import xml.etree.ElementTree
+import xml.etree.ElementTree as tree
 import xbmcaddon, xbmcplugin, xbmcgui
 import resources.lib.util as util
 
 handle = int(sys.argv[1])
-xbmcplugin.setContent(handle, 'TVShows')
-
-sysarg=str(sys.argv[1])
-ADDON_ID='plugin.video.nakamoriplugin'
-addon = xbmcaddon.Addon(id=ADDON_ID)
-
-rootDir = addon.getAddonInfo('path')
-if rootDir[-1] == ';':
-    rootDir = rootDir[0:-1]
-rootDir = xbmc.translatePath(rootDir)
-resDir = os.path.join(rootDir, 'resources')
-imgDir = os.path.join(resDir, 'images')
+xbmcplugin.setContent(handle, 'tvshows')
+addon = xbmcaddon.Addon(id='plugin.video.nakamoriplugin')
 
 urlopen = urllib2.urlopen
 Request = urllib2.Request
-opener = urllib2.build_opener()
-urllib2.install_opener(opener)
 
 def toInt(val):
      if val is None:
@@ -28,41 +16,36 @@ def toInt(val):
      return int(val)
 
 def buildMainMenu():
-    e=xml.etree.ElementTree.XML(getHtml("http://" + addon.getSetting("ipaddress") + ":" + addon.getSetting("port") + "/jmmserverkodi/getfilters/" + addon.getSetting("userid"),""))
+    xbmcplugin.setContent(handle, content='tvshows')
+    e=tree.XML(getHtml("http://" + addon.getSetting("ipaddress") + ":" + addon.getSetting("port") + "/jmmserverkodi/getfilters/" + addon.getSetting("userid"),""))
     for atype in e.findall('Directory'):
-        rating=9.0
-        label2="la2"
         title=atype.get('title')
         url=atype.get('key')
         thumb=atype.get('thumb')
         u=sys.argv[0]+"?url="+url+"&mode="+str(2)+"&name="+urllib.quote_plus(title)
-        ok=True
         #ListItem([label, label2, iconImage, thumbnailImage, path])
-        liz=xbmcgui.ListItem(label=title, label2="666", iconImage="DefaultVideo.png",  thumbnailImage=thumb, path=url)
-        #liz.setInfo( type="Video", infoLabels={ "Title": title, 'Rating': rating ,'episode': 3, 'playcount': 1 , 'watched': True, 'votes': 100} )
-        liz.setInfo( type="Video", infoLabels={ "Title": title, 'Rating': rating} )
+        liz=xbmcgui.ListItem(label=title, label2=title, iconImage="DefaultVideo.png",  thumbnailImage=thumb, path=url)
+        liz.setInfo( type="Video", infoLabels={ "Title": title } )
         liz.setProperty("Poster_Image", thumb)
-        #liz.setProperty("Label2", label2)
-        #liz.setProperty("Episode", str(1))
-        #liz.setLabel2(str(1))
-        xbmcplugin.addDirectoryItem(handle,url=u,listitem=liz,isFolder=True, totalItems=5)
+        liz.setProperty("Episode", str(1))
+        xbmcplugin.addDirectoryItem(handle,url=u,listitem=liz,isFolder=True)
     util.addDir("Search", "http://" + addon.getSetting("ipaddress") + ":" + addon.getSetting("port") + "/jmmserverkodi/search/" + addon.getSetting("userid") + "/"+ addon.getSetting("maxlimit") +"/", 3, "1","2","3","4")
     xbmcplugin.endOfDirectory(handle)
 
-def buildSubMenu(params):
-    e=xml.etree.ElementTree.XML(getHtml(params['url'],''))
-    #whenever there is series/movie its shown as <Directory>
+
+def buildSeriesMenu(params):
+    e=tree.XML(getHtml(params['url'],''))
     for atype in e.findall('Directory'):
         url=atype.get('key')
         thumb=atype.get('thumb')
         #Extended support
-        #count
-        #size
+        count=2
+        size=3
         #date
         try:
             genre=atype.find('Tag').get('tag')
         except:
-            genere="none"
+            genre="none"
         year=toInt(atype.get('year'))
         episode=toInt(atype.get('leafCount'))
         season=toInt(atype.get('season'))
@@ -78,19 +61,22 @@ def buildSubMenu(params):
         plot=atype.get('summary')
         plotoutline=plot
         title=atype.get('title')
-        originaltitle=atype.get('original_title')
+        try:
+            originaltitle=atype.get('original_title').encode("utf-8")
+        except:
+            originaltitle=title
         sorttitle=title.encode("utf-8")
         #duration=str(datetime.timedelta(minutes=((int(atype.find('Media').get('duration')))/60000)))[:-3]
         #studio="Ghilbi"
         #tagline="??"
         #writer=""
-        #tvshowtitle=""
+        tvshowtitle=title
         premiered = atype.get('originallyAvailableAt')
-        status = "Airing"
+        #status = "Airing"
         #code = ""
-        aired = "2008-12-07"
+        #aired = "2008-12-07"
         #credits
-        lastplayed = "2009-04-05 23:16:04"
+        #lastplayed = "2009-04-05 23:16:04"
         #album
         #artist
         votes = atype.get('votes')
@@ -99,8 +85,11 @@ def buildSubMenu(params):
         #Extended support END#
         u=sys.argv[0]+"?url="+url+"&mode="+str(2)+"&name="+urllib.quote_plus(title)+"&poster_file="+urllib.quote_plus(thumb)+"&filename="+urllib.quote_plus("none")
         liz=xbmcgui.ListItem(title, iconImage="DefaultVideo.png", thumbnailImage=thumb)
-        liz.setInfo( type="Video" , infoLabels={
-        'Title': title.encode("utf-8") , 
+        liz.setInfo( type="Video", infoLabels={
+        'Count': count,
+        'Size': size,
+        #'Date': date, 
+        'Title': title.encode("utf-8"), 
         'Genre': genre,
         'Year': year,
         'Episode': episode,
@@ -116,13 +105,13 @@ def buildSubMenu(params):
         'Mpaa': mpaa,
         'Plot': plot,
         'Plotoutline': plotoutline,
-        'Originaltitle': originaltitle.encode("utf-8"),
+        'Originaltitle': originaltitle,
         'Sorttitle': sorttitle,
         #'Duration': duration,
         #'Studio':studio,
         #'Tagline': tagline,
         #'Writer': writer,
-        #'Tvshowtitle': tvshowtitle,
+        'Tvshowtitle': tvshowtitle,
         'Premiered': premiered,
         #'Status': status,
         #code : string (tt0110293, - IMDb code
@@ -135,64 +124,79 @@ def buildSubMenu(params):
         #trailer : string (/home/user/trailer.avi,
         'Dateadded': dateadded
         })
+        liz.setProperty('IsPlayable', 'False')
         #Let's set some arts
         #liz.setArt({ 'thumb': thumb, 'poster': poster, 'banner' : banner, 'fanart': fanart, 'clearart': clearart, 'clearlogo': clearlogo, 'landscape': landscape})
-        #liz.setProperty("Poster_Image",thumb)
-        liz.setProperty('Label2', '10')
+        liz.setProperty('TotalSeasons','2')
+        liz.setProperty('TotalEpisodes','2')
+        liz.setProperty('WatchedEpisodes','1')
+        liz.setProperty('UnWatchedEpisodes','1')
+        liz.setProperty('NumEpisodes','2')
+        liz.setProperty('TVShows.Count','3')
         xbmcplugin.addDirectoryItem(handle,url=u,listitem=liz,isFolder=True)
-    #whenever there is a <Video> inside <Directory> this will kick in with VideoSpecific values
+        #xbmcgui.Dialog().ok('PARRRA',str(liz.getProperty('NumEpisodes')))
+    xbmcplugin.endOfDirectory(handle)
+    xbmcplugin.setContent(handle, content='tvshows')
+
+
+def buildEpisodesMenu(params):
+    e=tree.XML(getHtml(params['url'],''))
     for atype in e.findall('Video'):
-        a2=atype.get('key')
-        a3=3
-        try:
-            a4=atype.get('thumb')
-        except:
-            a4="none"
-        try:
-            plot=atype.get('summary')
-        except:
-            plot="none"
-        a6="none"
-        a7="none"
+        url=atype.get('key')
+        thumb=atype.get('thumb')
         #Extended support
+        #count
+        size=toInt(atype.find('Media').find('Part').get('size'))
+        #date
         try:
             genre=atype.find('Tag').get('tag')
         except:
-            genre=""
-        try:
-            year=atype.get('year')
-        except:
-            year=""
-        try:
-            episode=atype.get('index')
-        except:
-            episode="0"
-        title=atype.get('title')
-        originaltitle=atype.get('original_title')
-        sorttitle=title.encode("utf-8")
-        duration=str(datetime.timedelta(minutes=((int(atype.find('Media').get('duration')))/60000)))[:-3]
-        premiered = atype.get('originallyAvailableAt')
+            genre="none"
+        year=toInt(atype.get('year'))
+        episode=toInt(atype.get('index'))
+        season=toInt(atype.get('season'))
+        #top250=toInt()
+        #tracknumber=toInt()
         rating=atype.get('rating')
-        votes = atype.get('votes')
-        season=atype.get('season')
+        playcount=toInt(atype.get('viewCount'))
+        #overlay : integer (2, - range is 0..8. See GUIListItem.h for values
+        #cast
+        #castandrole
+        #director
         mpaa=atype.get('contentRating')
+        plot=atype.get('summary')
         plotoutline=plot
+        title=atype.get('title')
+        try:
+            originaltitle=atype.get('original_title').encode("utf-8")
+        except:
+            originaltitle=title
+        sorttitle=title.encode("utf-8")
+        duration=int(atype.find('Media').get('duration'))/1000
+        #xbmcgui.Dialog().ok('Error',str(duration))
+        #studio="Ghilbi"
+        #tagline="??"
+        #writer=""
+        tvshowtitle=title
+        premiered = atype.get('originallyAvailableAt')
+        #status = "Airing"
+        #code = ""
+        #aired = "2008-12-07"
+        #credits
+        #lastplayed = "2009-04-05 23:16:04"
+        #album
+        #artist
+        votes = atype.get('votes')
+        #trailer
         dateadded = atype.get('addedAt')
-        #TODO
-        playcount="0"
-        #NOT IMPLEMENTED IN Contract_AniDBAnime
-        studio="Ghilbi"
-        tagline="??"
-        writer=""
-        tvshowtitle=""
-        status = "Airing"
-        aired = "2008-12-07"
-        lastplayed = "2009-04-05 23:16:04"
         #Extended support END#
-        u=sys.argv[0]+"?url=" + a2+"&play="+str(4)+"&name="+urllib.quote_plus(title.encode("utf-8"))+ "&thumb="+a4 +"&poster="+a7+"&filename="+urllib.quote_plus(atype.find('Media').find('Part').get('file'))
-        liz=xbmcgui.ListItem(title.encode("utf-8"), iconImage=a4, thumbnailImage=a4)
+        u=sys.argv[0]+"?url=" + url+"&mode="+str(1)+"&play="+str(1)+"&name="+urllib.quote_plus(title.encode("utf-8")) +"&poster="+thumb+"&filename="+urllib.quote_plus(atype.find('Media').find('Part').get('file'))
+        liz=xbmcgui.ListItem(str(episode) + ". " + title, iconImage="DefaultVideo.png", thumbnailImage=thumb)
         liz.setInfo( type="Video" , infoLabels={
-        "Title": title.encode("utf-8") , 
+        #'Count': count,
+        'Size': size,
+        #'Date': date, 
+        'Title': title.encode("utf-8") , 
         'Genre': genre,
         'Year': year,
         'Episode': episode,
@@ -214,7 +218,7 @@ def buildSubMenu(params):
         #'Studio':studio,
         #'Tagline': tagline,
         #'Writer': writer,
-        #'Tvshowtitle': tvshowtitle,
+        'Tvshowtitle': tvshowtitle,
         'Premiered': premiered,
         #'Status': status,
         #code : string (tt0110293, - IMDb code
@@ -227,6 +231,9 @@ def buildSubMenu(params):
         #trailer : string (/home/user/trailer.avi,
         'Dateadded': dateadded
         })
+        liz.setProperty('IsPlayable', 'true')
+        #Let's set some arts
+        #liz.setArt({ 'thumb': thumb, 'poster': poster, 'banner' : banner, 'fanart': fanart, 'clearart': clearart, 'clearlogo': clearlogo, 'landscape': landscape})
         #Information about streams inside video file
         for vtype in atype.find('Media').find('Part').findall('Stream'):
             stream=int(vtype.get('streamType'))
@@ -253,14 +260,25 @@ def buildSubMenu(params):
                     language = vtype.get('language')
                     liz.addStreamInfo('subtitle', { 'Language': language })
                 except:
-                    language = ""
+                    pass
             else:
                 #error
                 xbmcgui.Dialog().ok('Error',str("Something went wrong!"))
         xbmcplugin.addDirectoryItem(handle,url=u,listitem=liz,isFolder=False)
-    xbmcplugin.endOfDirectory(int(sysarg))
+    xbmcplugin.endOfDirectory(handle)
+    xbmcplugin.setContent(handle, content='episodes')
 
-        
+
+def openMenu(params):
+    e=tree.XML(getHtml(params['url'],''))
+    if e.find('Directory') is not None:
+        buildSeriesMenu(params)
+    elif e.find('Video') is not None:
+        buildEpisodesMenu(params)
+    else:
+        buildSeriesMenu(params)
+
+
 def search(url):
     try:
         term=util.searchBox()
@@ -270,41 +288,12 @@ def search(url):
         pass
     
 
-def getLink(url):
-    www = url
-    params2 = ""
-    link = ""
-    e=xml.etree.ElementTree.XML(getHtml(www,params2))
-    for atype in e.iter('Part'):
-        link = atype.get('key')
-    return link
 
-
-def getTitle(url):
-    www = url
-    params2 = ""
-    link = ""
-    e=xml.etree.ElementTree.XML(getHtml(www,params2))
-    for atype in e.findall('Video'):
-        link=atype.get('title')
-    return link
-
-    
-def getThumb(url):
-    www = url
-    params2 = ""
-    link = ""
-    e=xml.etree.ElementTree.XML(getHtml(www,params2))
-    for atype in e.findall('Video'):
-        link=atype.get('thumb')
-    return link
-        
-    
 def playVideo(params):
-    link = getLink(params['url'])
-    thumb = getThumb(params['url'])
-    title = getTitle(params['url'])
-    #xbmcgui.Dialog().ok('MODE=link',str(link))
+    e=tree.XML(getHtml(params['url'],""))
+    title = e.find('Video').get('title')
+    thumb= e.find('Video').get('thumb')
+    link = e.find('Video').find('Media').find('Part').get('key')
     util.playMedia(title, thumb, link, "Video")
 
 
@@ -329,12 +318,13 @@ try:
 except:
     mode=None
 
+
 if mode==1: #VIDEO
     #xbmcgui.Dialog().ok('MODE=1',str(parameters))
     playVideo(parameters)
 elif mode==2: #DIRECTORY
     #xbmcgui.Dialog().ok('MODE=2',str(parameters))
-    buildSubMenu(parameters)
+     openMenu(parameters)
 elif mode==3: #SEARCH
     #xbmcgui.Dialog().ok('MODE=3',str(parameters))
     search(parameters['url'])
