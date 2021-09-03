5102c1e4b96880069db5b5614964439ca11f7883
SteveMavic <pan69stefan@gmail.com>
2021-09-03 18:53:31 +0200

    Update api2.py
    Added Summaries; improved response handling from api requestes

--- api/shoko/v2/api2.py
+++ api/shoko/v2/api2.py
@@ -21,33 +21,46 @@ api_client = APIClient(api_address=address, api_port=port, api_version=version, 
 #
 # Auth
 #
-def login_user(user='', password='', device=''):
-    """Authenticate user with given username and password\n
-    Returns `AuthUser` object"""
+def login_user(auth: AuthUser) -> str:
+    """
+    Get an authentication token for the user.
+
+    Parameters
+    ---
+        auth : AuthUser
+            The authentication details for the user.
+    """
     data = {
-        "user": user,
-        "pass": password,
-        "device": device
+        "user": auth.user,
+        "pass": auth.password,
+        "device": auth.device
     }
     response = api_client.call(url='/api/auth', call_type=APIType.POST, data=data, auth=False)
-    return json.loads(response, object_hook=AuthUser.Decoder)
+    return json.loads(response)
 
-
-def delete_user_apikey(apikey):
+def delete_user_apikey(apikey: str):
     """
-    Delete user apikey
+    Delete an APIKey from the database.
+    
+    Parameters
+    ---
+        apikey : str
+            The API key to delete
     """
     query = {'apikey': apikey}
     return api_client.call(url='/api/auth', call_type=APIType.DELETE, query=query, auth=False)
 
-def change_user_password(password):
+def change_user_password(password: str):
     """
     Change user password
+
+    Parameters
+    ---
+        password : str
+            new password
     """
     data = f"{password}"
     return api_client.call(url='/api/auth/ChangePassword', call_type=APIType.POST, data=data)
-
-
 
 #
 # Common
@@ -61,82 +74,155 @@ def change_user_password(password):
 def cloud_list():
     # 501: Not Implemented
     return api_client.call(url='/api/cloud/list', call_type=APIType.GET)
+
 def cloud_count():
     # 501: Not Implemented
     return api_client.call(url='/api/cloud/count', call_type=APIType.GET)
+
 def cloud_add():
     # 501: Not Implemented
     return api_client.call(url='/api/cloud/add', call_type=APIType.POST)
+
 def cloud_delete():
     # 501: Not Implemented
     return api_client.call(url='/api/cloud/delete', call_type=APIType.POST)
+
 def cloud_import():
     # 501: Not Implemented
     return api_client.call(url='/api/cloud/import', call_type=APIType.POST)
+
 def filter(opts: QueryOptions = QueryOptions()):
-    return api_client.call(url='/api/filter', call_type=APIType.GET, query=opts.__dict__)
+    response = api_client.call(url='/api/filter', call_type=APIType.GET, query=opts.__dict__)
+    _json = json.loads(response)
+    return Filter.Decoder(_json)
+
 def group(opts: QueryOptions = QueryOptions()):
-    return api_client.call(url='/api/group', call_type=APIType.GET, query=opts.__dict__)
+    response = api_client.call(url='/api/group', call_type=APIType.GET, query=opts.__dict__)
+    _json = json.loads(response)
+    output: list[Group] = []
+    for group in _json:
+        output.append(Group.Decoder(group))
+    return output
+
 def group_watch(id: int):
     query = {
         'id': id
     }
     return api_client.call(url='/api/group/watch', call_type=APIType.GET, query=query)
+
 def group_search(opts: QueryOptions = QueryOptions()):
-    return api_client.call(url='/api/group/search', call_type=APIType.GET, query=opts.__dict__)
+    response = api_client.call(url='/api/group/search', call_type=APIType.GET, query=opts.__dict__)
+    _json = json.loads(response)
+    output: list[Group] = []
+    for group in _json:
+        output.append(Group.Decoder(group))
+    return output
+
 def cast_by_series(id: int):
     query = {
         'id': id
     }
-    return api_client.call(url='/api/cast/byseries', call_type=APIType.GET, query=query)
+    response = api_client.call(url='/api/cast/byseries', call_type=APIType.GET, query=query)
+    _json = json.loads(response)
+    output: list[Role] = []
+    for role in _json:
+        output.append(Role.Decoder(role))
+    return output
+
 def cast_search(opts: QueryOptions = QueryOptions()):
     response = api_client.call(url='/api/cast/search', call_type=APIType.GET, query=opts.__dict__)
     _json = json.loads(response)
     return Filter.Decoder(_json)
+
 def links_serie(id: int):
     query = {
         'id': id
     }
     return api_client.call(url='/api/links/serie', call_type=APIType.GET, query=query)
+
 def folder_list():
+    """
+    List all saved Import Folders
+    """
     response = api_client.call(url='/api/folder/list', call_type=APIType.GET)
     _json = json.loads(response)
     output: list[ImportFolder] = []
     for folder in _json:
         output.append(ImportFolder.Decoder(folder))
     return output
+
 def folder_count() -> Counter:
+    """
+    Get import folders count
+    """
     response = api_client.call(url='/api/folder/count', call_type=APIType.GET)
     _json = json.loads(response)
     return Counter.Decoder(_json)
+
 def folder_add(folder: ImportFolder) -> ImportFolder:
+    """
+    Add Folder to Import Folders Repository
+    """
     response = api_client.call(url='/api/folder/add', call_type=APIType.POST, data=folder.__dict__)
     _json = json.loads(response)
     return ImportFolder.Decoder(_json)
+
 def folder_edit(folder: ImportFolder) -> ImportFolder:
+    """
+    Edit folder giving fulll ImportFolder object with ID
+    """
     response = api_client.call(url='/api/folder/edit', call_type=APIType.POST, data=folder.__dict__)
     _json = json.loads(response)
     return ImportFolder.Decoder(_json)
+
 def folder_delete(id: int):
+    """
+    Delete Import Folder out of Import Folder Repository
+    """
     query = {
         'folderId': id
     }
     return api_client.call(url='/api/folder/delete', call_type=APIType.POST, query=query)
+
 def folder_import():
+    """
+    Run Import action on all Import Folders inside Import Folders Repository
+    """
     return api_client.call(url='/api/folder/import', call_type=APIType.GET)
+
 def folder_scan():
+    """
+    Scan All Drop Folders
+    """
     return api_client.call(url='/api/folder/scan', call_type=APIType.GET)
+
 def remove_missing_files():
+    """
+    Scans your import folders and remove files from your database that are no longer in your collection.
+    """
     return api_client.call(url='/api/remove_missing_files', call_type=APIType.GET)
+
 def stats_update():
+    """
+    Updates all series stats such as watched state and missing files.
+    """
     return api_client.call(url='/api/stats_update', call_type=APIType.GET)
+
 def mediainfo_update():
+    """
+    Updates all technical details about the files in your collection via running MediaInfo on them.
+    """
     # typo in API endpoint?
     # should be media? meta?
     # assumed to be mediainfo
     return api_client.call(url='/api/medainfo_update', call_type=APIType.GET)
+
 def hash_sync():
+    """
+    Sync Hashes - download/upload hashes from/to webcache
+    """
     return api_client.call(url='/api/hash/sync', call_type=APIType.GET)
+
 def foler_rescan(id: int):
     """Accoring to Shoko.Server source code:
     "Rescan ImportFolder (with given `id`) to recognize new episodes"
@@ -145,16 +231,19 @@ def foler_rescan(id: int):
         'id': id
     }
     return api_client.call(url='/api/rescan', call_type=APIType.GET, query=query)
+
 def rescan_unlinked():
     """Accoring to Shoko.Server source code:
     "files which have been hashed, but don't have an associated episode"
     """
     return api_client.call(url='/api/rescanunlinked', call_type=APIType.GET)
+
 def rescan_manual_links():
     """Accoring to Shoko.Server source code:
     "files which have been hashed, but don't have an associated episode"
     """
     return api_client.call(url='/api/rescanmanuallinks', call_type=APIType.GET)
+
 def rehash(id: int):
     """Accoring to Shoko.Server source code:
     "Rehash given files in given VideoLocal"
@@ -163,26 +252,34 @@ def rehash(id: int):
         'id': id
     }
     return api_client.call(url='/api/rehash', call_type=APIType.GET, query=query)
+
 def rehash_unlinked():
     """Accoring to Shoko.Server source code:
     "files which have been hashed, but don't have an associated episode"
     """
     return api_client.call(url='/api/rehashunlinked', call_type=APIType.GET)
+
 def rehash_manual_links():
     """Accoring to Shoko.Server source code:
     "files which have been hashed, but don't have an associated episode"
     """
     return api_client.call(url='/api/rehashmanuallinks', call_type=APIType.GET)
-def myid_get():
+
+def myid_get() -> dict:
     """Accoring to Shoko.Server source code:
     "Returns current user ID for use in legacy calls"
     """
-    return api_client.call(url='/api/myid/get', call_type=APIType.GET)
+    return json.loads(api_client.call(url='/api/myid/get', call_type=APIType.GET))
 
-def ping():
-    return api_client.call(url='/api/ping', call_type=APIType.GET)
+def ping() -> dict:
+    """
+    Check if connection and auth is valid without ask for real data
+    """
+    return json.loads(api_client.call(url='/api/ping', call_type=APIType.GET))
 
 
-api_client.apikey = login_user('default', '', 'api-v2-device').apikey
+apikey = login_user(AuthUser(user="default", password=""))
+api_client.apikey = apikey['apikey']
 print(api_client.apikey)
 
+print(cast_by_series(1))
