b32f32e2e471befa1bffeb61de42f0d839cd9f83
SteveMavic <pan69stefan@gmail.com>
2021-09-10 20:52:13 +0200

    added reading config from file
    along with 2 more api v2 endpoints coverage

--- api/shoko/v3/api3.py
+++ api/shoko/v3/api3.py
@@ -12,12 +12,13 @@ from api3models import *
 import json
 
 # read test config from file that is not sync with gh
-config = json.loads("config.json")
-address = config['address']
-port = config['port']
-version = config['version']
-apikey = config['apikey']
-timeout = config['timeout']
+with open("config.json") as file:
+    config = json.load(file)
+    address = config['address']
+    port = config['port']
+    version = config['version']
+    apikey = config['apikey']
+    timeout = config['timeout']
 
 api_client = APIClient(api_address=address, api_port=port, api_version=version, api_key=apikey, timeout=timeout)
 
